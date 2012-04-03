cd2095fd96b8b38a4a2ab6ca54fb9c8c18a3a37a
FrancisVarga <github@varga-net.com>
2012-04-04 01:25:02 +0200

    initial commit

---
+++ HelloWorld/src/useless.erl
@@ -0,0 +1,37 @@
+%% Author: francis
+%% Created: Apr 4, 2012
+%% Description: TODO: Add description to useless
+-module(useless).
+-compile(export_all).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([add/2, hello/1, export_some_stuff/0]).
+
+%%
+%% API Functions
+%%
+add(VAR1,VAR2) ->
+	io:format(VAR1 + VAR2).
+
+hello(NAME) ->
+	io:format("Hallo ~s \n", [NAME]).
+
+hello() ->
+	io:format("You are a fool! \n").
+
+fetchUrl(URL) ->
+	io:format("URL => ~s \n", [URL]).
+
+export_some_stuff() ->
+	hello("Francis"),
+	add(5, 10).
+
+%%
+%% Local Functions
+%%
